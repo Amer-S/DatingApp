445dc25a6929cb83f98f883799fff4e855f55dd2
Amer <keroukhnuiwfnyblzy@awdrt.com>
2020-08-12 16:56:27 +0300

    End of section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,8 @@ namespace DatingApp.API.Controllers
 {
     // http;localhost:5000/api/values
 
-    [Route("api/[controller]")]//attribute based routing
+    [Authorize]
+    [Route("api/[controller]")] //attribute based routing
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -21,6 +23,7 @@ namespace DatingApp.API.Controllers
 
         }
         // Get api/values
+        
         [HttpGet]
         public async Task <IActionResult> GetValues()
         {
@@ -31,6 +34,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
